4698019a14940a261ad460ff00b45317c98b8995
Juan Di Nella <juan.dinella@gmail.com>
2018-04-17 20:09:40 -0300

    added Service Worker

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,7 +1,12 @@
 import asyncFonts from './asyncFonts'
+import registerServiceWorker from './registerServiceWorker'
 import './scroll'
 
 asyncFonts({
   href: 'assets/css/fonts.css',
   observer: 'futura'
 })
+
+if (process.env.NODE_ENV === 'production') {
+  registerServiceWorker()
+}
