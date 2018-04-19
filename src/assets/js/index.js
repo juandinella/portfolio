5a1286a3875a2228f5a01b159f6a1bf5e5914f7b
Juan Di Nella <juan.dinella@gmail.com>
2018-04-18 21:37:17 -0300

    test service worker

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,8 +1,8 @@
-// import registerServiceWorker from './registerServiceWorker'
+import registerServiceWorker from './registerServiceWorker'
 import app from './app.js'
 
 app()
 
-// if (process.env.NODE_ENV === 'production') {
-//   registerServiceWorker()
-// }
+if (process.env.NODE_ENV === 'production') {
+  registerServiceWorker()
+}
