1f9987899700648f7b24a036c0f94ba047a9fc4b
Juan Di Nella <juan.dinella@gmail.com>
2018-04-19 14:38:12 -0300

    test canvas

--- src/assets/js/general/general.js
+++ src/assets/js/general/general.js
@@ -1,9 +1,11 @@
 import asyncFonts from '../tools/async-fonts'
 import lazyImages from './lazyImages'
 import scrollTo from './scroll'
+import canvas from './canvas'
 
 const general = () => {
   lazyImages()
+  canvas()
 
   asyncFonts({
     href: 'assets/css/fonts.css',
